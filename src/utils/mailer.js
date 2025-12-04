e3eda352451af475bd3bd3e0f02ba33d55712c59
davidDiaz-11 <al347754@edu.uaa.mx>
2025-12-04 02:45:25 -0600

    Update mailer

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,14 +1,20 @@
 const nodemailer = require("nodemailer");
 
+const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT || 587,
+  port: process.env.EMAIL_PORT,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false, // 👈 NECESARIO EN RAILWAY
+  },
 });
+
 
 async function sendEmail({ to, subject, text, html, attachments = [] }) {
   try {
