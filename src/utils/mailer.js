37813834ea1865ed2687f2ff8dbe941d923c4282
davidDiaz-11 <al347754@edu.uaa.mx>
2025-12-04 02:48:40 -0600

    Update maile 2do intentor

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,7 +1,5 @@
 const nodemailer = require("nodemailer");
 
-const nodemailer = require("nodemailer");
-
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
